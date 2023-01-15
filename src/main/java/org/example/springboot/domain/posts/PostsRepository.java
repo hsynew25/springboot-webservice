caf30d83c9bc2dcbb32b9e59e03522c6e3b81ee6
hsy <hsynew25@gmail.com>
2023-01-16 00:11:13 +0900

    화면 ( 게시글 등록, 조회 ,수정, 삭제) 구현

--- src/main/java/org/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/example/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package org.example.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //Posts 클래스로 DB 접근하게 해주는 레포지토리(Dao임, JPA에선 '레포지토리'라고 부름)
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY  p.id DESC ")
+    List<Posts> findAllDesc();
 }
